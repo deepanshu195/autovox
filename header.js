b17ff1141c42b5ec78da49e6e93bc80e91b4d91d
deepanshu195 <deepanshugarg195@gmail.com>
2020-11-28 00:30:44 +0530

    feat(form): remainging

---
+++ header.js
@@ -0,0 +1,21 @@
+window.onscroll = function () {
+  myFunction();
+};
+
+let header = document.getElementById("myHeader");
+
+let sticky = header.offsetTop + 64;
+
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//     header.classList.add("background-transparent");
+//   } else {
+//     header.classList.remove("sticky");
+//     header.classList.remove("background-transparent");
+//   }
+// }
+window.onload = function () {
+  //   header.classList.add("sticky");
+  //   header.classList.add("background-transparent");
+};
